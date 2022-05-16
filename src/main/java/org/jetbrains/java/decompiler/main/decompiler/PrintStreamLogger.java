51cc0fa17e76a567116c128fb895c17b739426c0
Pfannekuchen <gamesupport@mgnet.work>
2022-05-16 21:13:30 +0200

    Cleaning up this mess.

---
+++ src/main/java/org/jetbrains/java/decompiler/main/decompiler/PrintStreamLogger.java
@@ -0,0 +1,16 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.java.decompiler.main.decompiler;
+
+import org.jetbrains.java.decompiler.main.extern.IFernflowerLogger;
+
+public class PrintStreamLogger extends IFernflowerLogger {
+
+	@Override
+	public void startClass(String className) {
+		System.out.println("Decompiling " + className + "...");
+	}
+
+	@Override public void writeMessage(String message, Severity severity) {}
+	@Override public void writeMessage(String message, Severity severity, Throwable t) { }
+	
+}
